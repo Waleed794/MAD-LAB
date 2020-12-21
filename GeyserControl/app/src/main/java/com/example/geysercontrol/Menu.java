8dc8b665c5c14901ac8651882f7f597673fbef7c
Waleed794 <53418492+Wali794@users.noreply.github.com>
2020-12-21 17:24:20 +0500

    Update

--- GeyserControl/app/src/main/java/com/example/geysercontrol/Menu.java
+++ GeyserControl/app/src/main/java/com/example/geysercontrol/Menu.java
@@ -1,19 +1,160 @@
 package com.example.geysercontrol;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Menu extends AppCompatActivity {
+
+    TextView name,mail;
+    Button btn_logout;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mRef;
+    private DatabaseReference mChild;
+    DeviceInfo deviceInfo = new DeviceInfo();
+    private RadioButton radioButton;
+    private RadioGroup radioGroup;
+    private boolean radioBool=true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_menu);
+        Intent intent = getIntent();
+        String deviceID=intent.getStringExtra("deviceID");
+
+        btn_logout=findViewById(R.id.button8);
+        name=findViewById(R.id.textView11);
+        mail=findViewById(R.id.textView12);
+
+        GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
+
+
+        mDatabase = FirebaseDatabase.getInstance();
+        mRef= mDatabase.getReference("Devices");
+        mChild=mRef.child(deviceID);
+        mChild.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                showData(snapshot);
+                viewData();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        //String userID= signInAccount.getId();
+        //String userName= signInAccount.getDisplayName();
+        //mRef.child(userID).setValue(userName);
+        /*mRef.child(userID).child("device1").setValue(true);
+        mRef.child(userID).child("device2").setValue(true);*/
+
+
+
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(),Login.class);
+                startActivity(intent);
+            }
+        });
+       // radioButton = findViewById(R.id.radio_hot);
+        radioGroup=findViewById(R.id.radioGroup);
+
     }
+
+
+    private void viewData() {
+        name.setText(deviceInfo.getMotor());
+        mail.setText(deviceInfo.getTemp());
+    }
+
+    private void showData(DataSnapshot snapshot) {
+
+
+            deviceInfo.setMotor(snapshot.child("Motor").getValue(String.class));
+            deviceInfo.setTemp(snapshot.child("Temp").getValue(String.class));
+            String str = deviceInfo.getMotor();
+            radioInit(deviceInfo.getMotor());
+
+         /*   Log.e("Motor",str);*/
+
+
+    }
+
     public void btn_SetSchedule(View view) {
         startActivity(new Intent(getApplicationContext(),schedule.class));
+    }
+
+
+    public void radioClick(View view) {
+        int radioId=radioGroup.getCheckedRadioButtonId();
+        radioButton=findViewById(radioId);
+        Log.e("radio id: ",String.valueOf(radioId));
+        switch (radioId)
+        {
+            case R.id.radio_Warm:
+                Toast.makeText(this, "Task Successful", Toast.LENGTH_SHORT).show();
+                mChild.child("Motor").setValue("1");
+                break;
+
+            case R.id.radio_Hot:
+                Toast.makeText(this, "Task Successful", Toast.LENGTH_SHORT).show();
+                mChild.child("Motor").setValue("2");
+                break;
+
+            case R.id.radio_veryhot:
+                Toast.makeText(this, "Task Successful", Toast.LENGTH_SHORT).show();
+                mChild.child("Motor").setValue("3");
+                break;
+
+        }
+    }
+
+    private void radioInit(String motor)
+    {
+        if(radioBool)
+        {
+            radioBool=false;
+            switch (motor)
+            {
+                case "1":
+                    radioButton=findViewById(R.id.radio_Warm);
+                    radioButton.setChecked(true);
+                    break;
+                case "2":
+                    radioButton=findViewById(R.id.radio_Hot);
+                    radioButton.setChecked(true);
+                    break;
+                case "3":
+                    radioButton=findViewById(R.id.radio_veryhot);
+                    radioButton.setChecked(true);
+                    break;
+            }
+        }
+
     }
 }
--- GeyserControl/app/src/main/java/com/example/geysercontrol/SignUp.java
+++ GeyserControl/app/src/main/java/com/example/geysercontrol/SignUp.java
@@ -1,14 +1,115 @@
 package com.example.geysercontrol;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class SignUp extends AppCompatActivity {
-
+    private ListView myListView;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mRef;
+    private DatabaseReference mChild;
+    private ArrayList<String> deviceList=new ArrayList<>();;
+    private ArrayList<String> deviceList_key=new ArrayList<>();;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Intent Deviceintent= getIntent();
         setContentView(R.layout.activity_sign_up);
+        //ArrayList<String> arrayList = (ArrayList<String>) Deviceintent.getSerializableExtra("deviceList");
+//        Log.e("check list",arrayList.toString());
+
+
+        final ArrayAdapter arrayAdapter = new ArrayAdapter<>(SignUp.this, android.R.layout.simple_list_item_1,deviceList);
+        myListView = (ListView) findViewById(R.id.myListView);
+        myListView.setAdapter(arrayAdapter);
+//         String str = arrayList.toString();
+         //Toast.makeText(SignUp.this, str, Toast.LENGTH_SHORT).show();
+        GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
+        signInAccount = GoogleSignIn.getLastSignedInAccount(this);
+        mDatabase = FirebaseDatabase.getInstance();
+        mRef= mDatabase.getReference("Users");
+        mChild=mRef.child(signInAccount.getId());
+
+
+        mChild.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                String str = snapshot.getValue(String.class);
+                deviceList.add(str);
+                String str_key= snapshot.getKey().toString();
+                deviceList_key.add(str_key);
+                arrayAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+               // Toast.makeText(SignUp.this, deviceList_key.get(position).toString(), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(),Menu.class);
+                //String str = deviceList_key.get(position).toString();
+                intent.putExtra("deviceID", deviceList_key.get(position));
+                startActivity(intent);
+                //callIntent(deviceList_key.get(position));
+
+            }
+        });
     }
+
+    private void callIntent(String position)
+    {
+        Intent intent = new Intent (getApplicationContext(),Menu.class);
+        intent.putExtra("position",position);
+        startActivity(intent);
+        finish();
+
+
+
+    }
+
+
+
 }
